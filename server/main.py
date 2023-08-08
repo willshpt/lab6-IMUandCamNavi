a0a76070c4ff2324ab0234a4fc1f94153b6ec4fd
willshpt <will@4phillips.com>
2023-08-08 16:33:34 -0500

    Added quaternion stuff

--- server/main.py
+++ server/main.py
@@ -2,6 +2,20 @@ from flask import Flask, send_from_directory
 from flask_cors import CORS
 import json
 
+
+import time
+import board
+import busio
+from adafruit_bno08x import BNO_REPORT_ROTATION_VECTOR
+
+from adafruit_bno08x.i2c import BNO08X_I2C
+
+i2c = busio.I2C(board.SCL, board.SDA)
+bno = BNO08X_I2C(i2c)
+
+bno.enable_feature(BNO_REPORT_ROTATION_VECTOR)
+
+
 #g.imu_x = 0
 imu_y = 0
 imu_z = 0
@@ -13,14 +27,24 @@ app = Flask(__name__,
 app.config['x'] = 0
 CORS(app)
 
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Hello, World!</p> <a href=\"index.html\">Click here for graphics</a>"
+
 
 @app.route("/static/<path:path>")
 def webglfun(path):
     #print(path)
     return send_from_directory('static',path)
+    
+@app.route("/getQuaternion")
+def getQuaternion():
+    #print(path)
+    quat_i, quat_j, quat_k, quat_real = bno.quaternion
+    return "Rotation Vector Quaternion:" + \
+        "I: %0.6f  J: %0.6f K: %0.6f  Real: %0.6f" % (quat_i, quat_j, quat_k, quat_real)
+    
 
 @app.route("/getimu")
 def get_imu():
