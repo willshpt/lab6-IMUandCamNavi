fb2a4c75481a6531310e71118a61332dc0e952e9
willshpt <Will@4phillips.com>
2023-08-24 18:16:44 -0500

    Oops

--- server/main.py
+++ server/main.py
@@ -1,7 +1,9 @@
 from flask import Flask, send_from_directory
 from flask_cors import CORS
 import json
-
+import urllib.request
+from PIL import Image
+from gesture_recognition import get_results
 
 import time
 import board
